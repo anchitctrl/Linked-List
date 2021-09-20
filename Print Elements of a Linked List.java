1dac498d49c76e1092c9ded5c000c3d6b4836d43
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-09-20 16:43:13 +0530

    Create Print Elements of a Linked List.java

---
+++ Print Elements of a Linked List.java
@@ -0,0 +1,8 @@
+public void display() {
+		ListNode current = head;
+		while(current != null) {
+			System.out.print(current.data + " --> ");
+			current = current.next;
+		}
+		System.out.print("null");
+	}
