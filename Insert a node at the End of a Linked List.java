cc8318d4422775a81173d56a7664681c05a91095
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-09-20 16:44:36 +0530

    Create Insert a node at the End of a Linked List.java

---
+++ Insert a node at the End of a Linked List.java
@@ -0,0 +1,12 @@
+public void insertLast(int value) {
+		ListNode newNode = new ListNode(value);
+		if (head == null) {
+			head = newNode;
+			return;
+		}
+		ListNode current = head;
+		while (current.next != null) {
+			current = current.next;
+		}
+		current.next = newNode;
+	}
