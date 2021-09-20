a92b87fd338b3777a9c3ea36797fe521fddf7622
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-09-20 16:47:37 +0530

    Create Find the Middle node in a Linked List.java

---
+++ Find the Middle node in a Linked List.java
@@ -0,0 +1,13 @@
+public ListNode getMiddleNode() {
+		if(head == null) {
+			return null;
+		}
+		ListNode slowPtr = head;
+		ListNode fastPtr = head;
+		
+		while(fastPtr != null && fastPtr.next != null) {
+			slowPtr = slowPtr.next;
+			fastPtr = fastPtr.next.next;
+		}
+		return slowPtr;
+	}
