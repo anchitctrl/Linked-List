2d60db92c0eb85d69e796351c44126b0dfb9a6e8
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-09-20 16:43:43 +0530

    Create Length of a Linked List.java

---
+++ Length of a Linked List.java
@@ -0,0 +1,12 @@
+public int length() {
+		if(head == null) {
+			return 0;
+		}
+		int count = 0;
+		ListNode current = head;
+		while(current != null) {
+			count++;
+			current = current.next;
+		}
+		return count;
+	}
